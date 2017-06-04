75bf81f66ad34b836b7ff9c83d445e687c676c6b
wallacyyy <leonardowrc@gmail.com>
2017-06-04 02:21:40 +0200

    Finish refactor

--- src/coffeepoll.js
+++ src/coffeepoll.js
@@ -1,19 +1,9 @@
-// Description:
-//   Help your team to find a place to drink a coffee !
-//   Coffeepoll will create poll with random coffee shops nearby.
-//
-// Commands:
-//   hubot coffeepoll near <text> - Configure the place for next polls
-//   hubot coffeepoll start - Start the poll
-//   hubot coffeepoll vote <number> - Vote in one of poll options
-//   hubot coffeepoll partial - Show the partial results
-//   hubot coffeepoll finish - Finish the poll
-
 if (process.env.NODE_ENV !== 'production') {
   // eslint-disable-next-line global-require
   require('dotenv').config();
 }
 
+const R = require('ramda');
 const _ = require('lodash');
 const messages = require('../lib/messages');
 const venues = require('node-foursquare-venues');
@@ -23,114 +13,248 @@ const foursquare = venues(
   process.env.FOURSQUARE_CLIENT_SECRET
 );
 
+const INITIAL_STATE = {
+  near: 'Berlin',
+  radius: 500,
+  participants: {},
+  options: [],
+  votes: []
+};
+
+/**
+ * Help your team to find a place to drink a coffee !
+ * Coffeepoll will create poll with random coffee shops nearby.
+ *
+ * @example
+ * hubot coffeepoll near <text> - Configure the place for next polls
+ * hubot coffeepoll start - Start the poll
+ * hubot coffeepoll vote <number> - Vote in one of poll options
+ * hubot coffeepoll partial - Show the partial results
+ * hubot coffeepoll finish - Finish the poll
+ */
 module.exports = (bot) => {
-  const brain = bot.brain;
-  const options = [];
-  const votes = [];
-  const participants = {};
+  const { brain } = bot;
 
-  brain.set('near', 'Berlin');
-  brain.set('radius', 500);
-  brain.set('participants', participants);
-  brain.set('options', options);
-  brain.set('votes', votes);
+  brain.set(INITIAL_STATE);
 
-  const clearPoll = () => {
-    options.length = 0;
-    votes.length = 0;
-    _.mapKeys(participants, (v, k) => {
-      delete participants[k];
-    });
+  const isPollStarted = R.pipe(() => brain.get('options'), o => R.gt(o.length, 0));
+
+  const isUserAlreadyVoted = username => (
+    R.pipe(
+      () => brain.get('participants'),
+      p => !!R.prop(username, p)
+    )()
+  );
+
+  const isVoteInvalid = voteIndex => (
+    R.pipe(
+      () => brain.get('votes'),
+      votes => votes[voteIndex],
+      R.isNil
+    )(voteIndex)
+  );
+
+  const handleSearchError = (error, near, res) => (
+    R.ifElse(
+      e => R.equals(e, 400),
+      () => res.send(messages.errorPlaceNotFound(near)),
+      () => res.send(error)
+    )(error)
+  );
+
+  const handleVenues = (sample, res) => {
+    R.pipe(
+      smp => (
+        R.reduce(
+          (acc, cs) => {
+            acc.message += `\n${sample.indexOf(cs)}: ${cs.name} (${messages.url}${cs.id})\n`;
+            acc.message += `${cs.location.address}\n\n`;
+            acc.options.push(cs);
+            acc.votes.push(0);
+
+            return acc;
+          },
+          {
+            options: [],
+            votes: [],
+            message: ''
+          },
+          smp
+        )
+      ),
+      (st) => {
+        brain.set('options', st.options);
+        brain.set('votes', st.votes);
+        res.send(st.message);
+      }
+    )(sample);
   };
 
-  const isPollNotStarted = () => _.isEmpty(options);
-  const isUserAlreadyVoted = username => participants[username];
-  const isVoteNotValid = vote => (typeof votes[vote] === 'undefined');
-  const isRadiusNotValid = radius => !(radius > 0);
+  const searchVenues = params => (
+    new Promise((resolve, reject) => (
+      foursquare.venues.search(params, (error, payload) => (
+        R.ifElse(
+          R.isNil,
+          () => resolve(payload),
+          () => reject(error)
+        )(error)
+      )
+    ))
+  ));
 
-  bot.respond(/(?:coffeepoll|coffepoll) near (.*)/i, (res) => {
-    const place = res.match[1];
+  const updateVotes = voteIndex => (
+    R.pipe(
+      () => brain.get('votes'),
+      v => R.adjust(R.add(1), voteIndex, v),
+      incV => brain.set('votes', incV)
+    )()
+  );
 
-    brain.set('near', place);
+  const updateParticipants = (username, res) => (
+    R.pipe(
+      () => brain.get('participants'),
+      p => R.update(true, username, p),
+      p => R.merge(p, { [username]: true }),
+      updated => brain.set('participants', updated),
+      () => res.send(messages.thanks)
+    )()
+  );
 
-    return res.send(messages.places(place));
-  });
+  const buildPartial = data => (
+    R.reduce(
+      (msg, opt) => (
+        R.concat(
+          msg,
+          `${opt.name}: ${data.votes[data.options.indexOf(opt)]} vote(s)\n`
+        )
+      ),
+      messages.partial,
+      data.options
+    )
+  );
+
+  bot.respond(/(?:coffeepoll|coffepoll) near (.*)/i, res => (
+    R.pipe(
+      r => r.match[1],
+      (place) => {
+        brain.set('near', place);
+        res.send(messages.places(place));
+      }
+    )(res)
+  ));
+
+  bot.respond(/(?:coffeepoll|coffepoll) radius (.*)/i, res => (
+    R.pipe(
+      r => r.match[1],
+      radius => R.ifElse(
+        r => R.gt(r, 0),
+        () => {
+          const roundedRadius = Math.round(parseInt(radius, 10));
+          brain.set('radius', roundedRadius);
+          res.send(messages.radiusUpdated(roundedRadius));
+        },
+        () => res.send(messages.errorRadiusNotValid)
+      )(radius)
+    )(res)
+  ));
 
   bot.respond(/(?:coffeepoll|coffepoll) start/i, (res) => {
-    if (!isPollNotStarted()) return res.send(messages.errorAlreadyStarted);
-
-    const near = brain.get('near');
-    const radius = brain.get('radius');
-
     const params = {
-      near,
+      near: brain.get('near'),
       categoryId: messages.category,
-      radius
+      radius: brain.get('radius')
     };
 
-    return foursquare.venues.search(params, (error, payload) => {
-      if (error === 400) return res.send(messages.errorPlaceNotFound(near));
-      if (error) return res.send(error);
-
-      let message = messages.hello(bot.name);
-      const coffeeShops = _.sample(payload.response.venues, 3);
-
-      for (let i = 0; i < coffeeShops.length; i += 1) {
-        const cs = coffeeShops[i];
-        message += `${i}: ${cs.name} (${messages.url}${cs.id})\n`;
-        message += `${cs.location.address}\n\n`;
-        options[i] = cs;
-        votes[i] = 0;
-      }
-
-      return res.send(message);
-    });
-  });
-
-  bot.respond(/(?:coffeepoll|coffepoll) radius (.*)/i, (res) => {
-    const radius = res.match[1];
-
-    if (isRadiusNotValid(radius)) return res.send(messages.errorRadiusNotValid);
-
-    brain.set('radius', parseInt(radius, 10));
-    return res.send(messages.radiusUpdated(brain.get('radius')));
+    R.ifElse(
+      isPollStarted,
+      () => res.send(messages.errorAlreadyStarted),
+      () => (
+        searchVenues(params)
+          .then(payload => handleVenues(
+            _.sample(payload.response.venues, 3), res)
+          )
+          .catch(e => handleSearchError(e, params.near, res))
+      )
+    )();
   });
 
   bot.respond(/(?:coffeepoll|coffepoll) vote (.*)/i, (res) => {
     const username = res.message.user.name.toLowerCase();
-    const number = res.match[1];
+    const voteIndex = res.match[1];
 
-    if (isUserAlreadyVoted(username)) return res.send(messages.errorAlreadyVoted(username));
-    if (isPollNotStarted()) return res.send(messages.errorStart(bot.name));
-    if (isVoteNotValid(number)) return res.send(messages.errorVoteNotFound);
-
-    votes[number] += 1;
-    participants[username] = true;
-
-    return res.send(messages.thanks);
+    R.cond([
+      [
+        () => isUserAlreadyVoted(username),
+        () => res.send(messages.errorAlreadyVoted(username))
+      ],
+      [
+        R.pipe(
+          isPollStarted,
+          R.not
+        ),
+        () => res.send(messages.errorStart(bot.name))
+      ],
+      [
+        () => isVoteInvalid(voteIndex),
+        () => res.send(messages.errorVoteNotFound)
+      ],
+      [
+        R.T,
+        () => {
+          updateVotes(parseInt(voteIndex, 10));
+          updateParticipants(username, res);
+        }
+      ]
+    ])();
   });
 
+  const pickWinner = votes => (
+    R.pipe(
+      R.sort((a, b) => a - b),
+      R.last,
+      greater => R.indexOf(greater, votes),
+      idx => brain.get('options')[idx]
+    )(votes)
+  );
+
   bot.respond(/(?:coffeepoll|coffepoll) finish/i, (res) => {
-    if (isPollNotStarted()) return res.send(messages.errorStart(bot.name));
-
-    const greater = _.last(votes.slice().sort());
-    const winner = options[_.indexOf(votes, greater)];
-    clearPoll();
-
-    return res.send(messages.win(winner));
+    R.ifElse(
+      R.pipe(
+        isPollStarted,
+        R.not
+      ),
+      () => res.send(messages.errorStart(bot.name)),
+      () => (
+        R.pipe(
+          () => brain.get('votes'),
+          pickWinner,
+          winner => res.send(messages.win(winner)),
+          () => brain.set(INITIAL_STATE)
+        )()
+      )
+    )();
   });
 
   bot.respond(/(?:coffeepoll|coffepoll) help/i, res => res.send(messages.help));
 
   bot.respond(/(?:coffeepoll|coffepoll) partial/i, (res) => {
-    if (isPollNotStarted()) return res.send(messages.errorStart(bot.name));
+    const data = {
+      options: brain.get('options'),
+      votes: brain.get('votes')
+    };
 
-    let message = messages.partial;
-
-    for (let i = 0; i < options.length; i += 1) {
-      message += `${options[i].name}: ${votes[i]} vote(s)\n`;
-    }
-
-    return res.send(message);
+    return R.ifElse(
+      R.pipe(
+        isPollStarted,
+        R.not
+      ),
+      () => res.send(messages.errorStart(bot.name)),
+      () => (
+        R.pipe(
+          buildPartial,
+          partial => res.send(partial)
+        )(data)
+      )
+    )();
   });
 };
